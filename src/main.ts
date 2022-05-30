d039361032e3df66159c15f6f728f89aa242a073
olcay <olcayusta02@gmail.com>
2022-05-30 20:45:45 +0300

    Initial fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableProdMode, importProvidersFrom } from "@angular/core";
+import { environment } from "./environments/environment";
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { RouterModule } from "@angular/router";
+import { routes } from "./app/app.routes";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+      RouterModule.forRoot(routes),
+      ServiceWorkerModule.register("ngsw-worker.js", {
+        enabled: environment.production,
+        registrationStrategy: "registerWhenStable:30000",
+      })
+    ),
+  ],
+}).catch((reason) => console.error(reason));
